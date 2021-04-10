5903499737a260421fb6ed6759c5f68486ba595f
BeLazy167 <dhruvkhara167@gmail.com>
2021-04-10 23:15:34 +0530

    small changes

--- strategy2.py
+++ strategy2.py
@@ -11,27 +11,28 @@ exchange = ccxt.binance(
         'secret':config.apiSecret,
     }
 )
-while True:
-    candles = exchange.fetch_ohlcv('BTC/USDT',timeframe='5m',limit = 21)
-    df = pd.DataFrame(candles[:20], columns=['timestam','o','h','l','c','vol'])
-    rsi = RSIIndicator(df['c'])
-    bb = BollingerBands(df['c'])
-    df["ub"] ,df["lb"]  ,df["bbma"],df['rsi']  = bb.bollinger_hband() , bb.bollinger_lband(), bb.bollinger_mavg() ,rsi.rsi()
-    finalData = df.tail(1).values.tolist()[0]
-
-    if finalData[9]<40 and finalData[1]<df[7] and df[7]<df[4]:
-        print('enter')
-        break
-    else:
-        print('not yet')
-    # epoch = int(finalData[0])
-    # print(epoch)
-    # print(datetime.fromtimestamp(epoch))
-    time.sleep( 50 )
+def tradefinder():
+    while True:
+        candles = exchange.fetch_ohlcv('BTC/USDT',timeframe='5m',limit = 21)
+        df = pd.DataFrame(candles[:20], columns=['timestam','o','h','l','c','vol'])
+        rsi = RSIIndicator(df['c'])
+        bb = BollingerBands(df['c'])
+        df["ub"] ,df["lb"]  ,df["bbma"],df['rsi']  = bb.bollinger_hband() , bb.bollinger_lband(), bb.bollinger_mavg() ,rsi.rsi()
+        finalData = df.tail(1).values.tolist()[0]
+        print(df)
+        if finalData[9]<40 and finalData[1]<df[7] and df[7]<df[4]:
+            print('enter')
+            break
+        else:
+            print('not yet')
+        epoch = (finalData[0]//1000)+300
+        print(epoch)
+        print(time.time())
+        time.sleep( 50 )
 # c = df.loc[df['rsi']<=50]
 # x = c.loc[df['o']<df['lb']]
 # y = x.loc[df['c']>df['lb']]
-
+tradefinder()
 # print(y)
 
 
