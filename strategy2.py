400292ec0c0330adf83a5a65d17ba9b8adf27938
BeLazy167 <66153057+BeLazy167@users.noreply.github.com>
2021-04-10 16:08:46 +0530

    Minor Changes in Code

--- strategy2.py
+++ strategy2.py
@@ -1,6 +1,7 @@
 import ccxt
 import pandas as pd
 from ta.volatility import BollingerBands
+from ta.momentum import RSIIndicator
 import config
 
 exchange = ccxt.binance(
@@ -9,12 +10,21 @@ exchange = ccxt.binance(
         'secret':config.apiSecret,
     }
 )
-candles = exchange.fetch_ohlcv('BTC/USDT',limit = 50)
+candles = exchange.fetch_ohlcv('BTC/USDT',timeframe='5m',limit = 1000)
 df = pd.DataFrame(candles, columns=['timestam','o','h','l','c','vol'])
+rsi = RSIIndicator(df['c'])
 bb = BollingerBands(df['c'])
-print(bb)
-df["ub"] ,df["lb"]  ,df["bbma"]  = bb.bollinger_hband() , bb.bollinger_lband(), bb.bollinger_mavg()
-print(df[12]['o'])
+df["ub"] ,df["lb"]  ,df["bbma"],df['rsi']  = bb.bollinger_hband() , bb.bollinger_lband(), bb.bollinger_mavg() ,rsi.rsi()
+
+c = df.loc[df['rsi']<=50]
+print(c)
+x = c.loc[df['o']<df['lb']]
+y = x.loc[df['c']>df['lb']]
+
+print(y)
+
+
+
 # for x in range(len(df)):
 #     if df[x]['o']<df[x]['lb'] and df[x]['lb']<df[x]['c']:
 #         print(x,df[x]['o'],df[x]['lb'],df[x]['c'])    
