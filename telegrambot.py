1d0b64e0f14139bc42c59c424f95fa1b8ab6bcb0
BeLazy167 <dhruvkhara167@gmail.com>
2021-04-11 16:28:00 +0530

    pass

--- telegrambot.py
+++ telegrambot.py
@@ -1,5 +1,6 @@
 import telebot 
 import config
+
 bot = telebot.TeleBot(config.telegramApi, parse_mode=None) # You can set parse_mode by default. HTML or MARKDOWN
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
