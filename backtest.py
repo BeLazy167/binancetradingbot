8504fe2fdad5fdb44d7f38ac4d10487fe36da60c
BeLazy167 <dhruvkhara167@gmail.com>
2021-04-11 20:11:42 +0530

    pass

--- backtest.py
+++ backtest.py
@@ -11,15 +11,19 @@ exchange = ccxt.binance(
         'secret':config.apiSecret,
     }
 )
-syms = ['BTC/USDT','ETH/USDT','XRP/USDT']
+sum =0
+syms = ['BTC/USDT','ETH/USDT','XRP/USDT','LTC/USDT','BCH/USDT','TRX/USDT',"EOS/USDT",'LINK/USDT',"BNB/USDT"]
 for sym in syms:
-        candles = exchange.fetch_ohlcv(sym,timeframe='5m',limit = 200)
+        candles = exchange.fetch_ohlcv(sym,timeframe='5m',limit = 2000)
         df = pd.DataFrame(candles, columns=['timestam','o','h','l','c','vol'])
         rsi = RSIIndicator(df['c'])
         bb = BollingerBands(df['c'])
-        df["ub"] ,df["lb"]  ,df["bbma"],df['rsi']  = bb.bollinger_hband() , bb.bollinger_lband(), bb.bollinger_mavg() ,rsi.rsi()
+        df["ub"] ,df["lb"]  ,df["bbma"],df['rsi'],df['time']  = bb.bollinger_hband() , bb.bollinger_lband(), bb.bollinger_mavg() ,rsi.rsi(), pd.to_datetime(df['timestam'],unit='ms')
+        
         c = df.loc[df['rsi']<40]
         x = c.loc[df['o']<df['lb']]
         y = x.loc[df['c']>df['lb']]
         print(sym)
         print(y)
+        sum = sum + y.size//11
+print(sum)
--- telegrambot.py
+++ telegrambot.py
@@ -5,5 +5,10 @@ bot = telebot.TeleBot(config.telegramApi, parse_mode=None) # You can set parse_m
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
 	bot.reply_to(message, "Howdy, how are you doing?")
+	print("done")
+	print(message.message_id)
+user = bot.get_me()
+print(user)
 
 bot.polling()
+
